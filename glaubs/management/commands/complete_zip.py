9f42d36f33d1b9fb22c1f1ab984e23ed9ae69b06
Patrick Stählin <me@packi.ch>
2016-05-07 14:48:45 +0200

    Script to complete the zip codes
    The first csv only has the main zip code so we're
    importing the other zip codes. The names won't match
    but that should be fine.

---
+++ glaubs/management/commands/complete_zip.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+from municipalities.models import Municipality
+import csv
+
+
+class Command(BaseCommand):
+    help = 'Completes zip-codes'
+
+    def add_arguments(self, parser):
+        parser.add_argument('municipality_file', type=str)
+
+    def handle(self, *args, **options):
+        with open(options['municipality_file'], 'r') as f:
+            csvf = csv.reader(f)
+
+            for idx, row in enumerate(csvf):
+                if idx == 0:
+                    continue
+
+                canton = row[3]
+                name = row[4]
+                bfs_number = row[2]
+                zip_code = row[0]
+                try:
+                    Municipality.objects.get(canton=canton, name=name, bfs_number=bfs_number, zip_code=zip_code)
+                except Municipality.DoesNotExist:
+                    Municipality.objects.create(canton=canton, name=name, bfs_number=bfs_number, zip_code=zip_code)
