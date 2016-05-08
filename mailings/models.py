ed146d622640b2e23ae3c3b454fff992f0b319b6
Patrick Stählin <me@packi.ch>
2016-05-08 23:52:54 +0200

    Set sent-date on creation

--- mailings/models.py
+++ mailings/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Mailing(models.Model):
     from_number = models.IntegerField(null=True)
     to_number = models.IntegerField(null=True)
-    sent_on = models.DateTimeField(null=True)
+    sent_on = models.DateTimeField(auto_now_add=True, null=True)
     received_on = models.DateTimeField(null=True)
     state = models.TextField(default='new')
     alert = models.BooleanField(default=False)
